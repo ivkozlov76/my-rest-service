763b66a80d9424758057b8571792bc1ff5bec7ff
Ivan Kozlov <kozlov.i.vladi@sberbank.ru>
2024-04-26 22:17:40 +0300

    init

---
+++ src/service/UserService.java
@@ -0,0 +1,9 @@
+package service;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/{userId}/bookmarks")
+public class UserService {
+}
